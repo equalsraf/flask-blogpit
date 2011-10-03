0c9aa615095ed0f006e3bdb17c8337533b0221fd
Rui Abreu Ferreira <raf-ep@gmx.com>
2011-10-03 14:12:58 +0100

    Limit author name length in comments

--- flaskext/blogpit/forms.py
+++ flaskext/blogpit/forms.py
@@ -1,9 +1,9 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
-from flaskext.wtf import Form, TextField, Required, TextAreaField
+from flaskext.wtf import Form, TextField, Required, TextAreaField, Length
 
 class CommentForm(Form):
-    name = TextField('name', validators=[Required()])
+    name = TextField('name', validators=[Required(), Length(max=100)])
     content = TextAreaField('content', validators=[Required()])
 
 
